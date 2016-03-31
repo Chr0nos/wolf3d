7b3c8ee91c93bc283f3103258842cc463df8d94e
Sébastien Nicolet <snicolet@student.42.fr>
2016-03-31 12:54:52 +0200

    ameliortions

--- textures_floor.c
+++ textures_floor.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/30 21:07:37 by snicolet          #+#    #+#             */
-/*   Updated: 2016/03/31 00:22:36 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/03/31 02:45:51 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,27 @@
 
 static void		dtf_init(t_context *c, const t_ray *ray, t_posxy *fl)
 {
-	t_point		map;
+	const t_posxy	*map = &c->player.pos;
 
-	map.x = (int)c->player.pos.x;
-	map.y = (int)c->player.pos.y;
 	if (ray->orientation == PO_N)
 	{
-		fl->x = map.x;
-		fl->y = map.y + ray->wallx;
+		fl->x = map->x;
+		fl->y = map->y + ray->wallx;
 	}
 	else if (ray->orientation == PO_S)
 	{
-		fl->x = map.x + 1.0;
-		fl->y = map.y + ray->wallx;
+		fl->x = map->x + 1.0;
+		fl->y = map->y + ray->wallx;
 	}
 	else if (ray->orientation == PO_E)
 	{
-		fl->x = map.x + ray->wallx;
-		fl->y = map.y;
+		fl->x = map->x + ray->wallx;
+		fl->y = map->y;
 	}
 	else
 	{
-		fl->x = map.x + ray->wallx;
-		fl->y = map.y + 1.0;
+		fl->x = map->x + ray->wallx;
+		fl->y = map->y + 1.0;
 	}
 }
 
@@ -57,15 +55,15 @@ void			display_tex_floor(t_context *c, const t_ray *ray,
 	px = wall->end;
 	while (px.y < c->x->height)
 	{
-		current_dist = h / (2 * px.y - h);
+		current_dist = h / (2.0 * px.y - h);
 		coefw = (current_dist - ray->dist) / (ray->dist - dist_player);
-		cfl.x = coefw * fl.x + (1.0 - coefw) * c->player.pos.x;
-		cfl.y = coefw * fl.y + (1.0 - coefw) * c->player.pos.y;
-		//draw_px(c->x, &px, texture_px(tex, draw_make_px(
-		//	((int)(cfl.x * tex->width) % tex->width),
-		//	((int)(cfl.y * tex->height) % tex->height)
-		//)));
-		draw_px(c->x, &px, COLOR_BROWN);
+		cfl.x = coefw * fl.x + (1.0 - coefw) * c->player.rootpos.x;
+		cfl.y = coefw * fl.y + (1.0 - coefw) * c->player.rootpos.y;
+		draw_px(c->x, &px, texture_px(tex, draw_make_px(
+			(int)(cfl.x * tex->width) % tex->width,
+			(int)(cfl.y * tex->height) % tex->height
+		)));
+		//draw_px(c->x, &px, COLOR_BROWN);
 		px.y++;
 	}
 }
